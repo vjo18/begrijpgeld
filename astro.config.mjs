04305174bb724d763c622e5815e21735b08b94e2
vjo18 <j_vercouter@hotmail.com>
2026-03-07 16:00:24 +0100

    Initial commit

---
+++ astro.config.mjs
@@ -0,0 +1,31 @@
+import { defineConfig } from "astro/config";
+import sitemap from "@astrojs/sitemap";
+import tailwindcss from "@tailwindcss/vite";
+import icon from "astro-icon";
+import remarkReadingTime from "remark-reading-time";
+
+export default defineConfig({
+  site: "https://astrostarterpro.com/",
+  integrations: [sitemap(), icon()],
+  markdown: {
+    remarkPlugins: [
+      remarkReadingTime,
+      () => {
+        return function (tree, file) {
+          file.data.astro.frontmatter.minutesRead =
+            file.data.readingTime.minutes;
+        };
+      },
+    ],
+  },
+  prefetch: {
+    prefetchAll: true,
+    defaultStrategy: "viewport",
+  },
+  build: {
+    inlineStylesheets: "always",
+  },
+  vite: {
+    plugins: [tailwindcss()],
+  },
+});
